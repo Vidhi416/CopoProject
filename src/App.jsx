cf4ca452f2d225204e65e662d61aeb6d1f0ec91e
Dheeraj-Pai <paidheeraj1402@gmail.com>
2025-07-10 16:36:17 +0530

    Change 1:Drawer and pages modification

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ import Home from './pages/Home';
 import Course1 from './pages/Course1';
 import Course2 from './pages/Course2';
 import Copomap from './pages/Copomap';
+import ViewCAM from './pages/ViewCAM';
+import UpRes from './pages/UpRes';
+import ViewReps from './pages/ViewReps';
 
 export default function App() {
   const [drawerOpen, setDrawerOpen] = useState(true);
@@ -19,6 +22,9 @@ export default function App() {
           <main className="flex-1 overflow-y-auto p-4">
             <Routes>
               <Route path="/" element={<Home />} />
+              <Route path="/ViewCAM" element={<ViewCAM />} />
+              <Route path="/UpRes" element={<UpRes />} />
+              <Route path="/ViewReps" element={<ViewReps />} />
               <Route path="/course1" element={<Course1 />} />
               <Route path="/course2" element={<Course2 />} />
               <Route path="/copomap" element={<Copomap />} />
